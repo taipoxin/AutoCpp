de2611019f972e9a5b6db88f0d34348565d95b13
Dmitry <dimontiranid1999@gmail.com>
2018-01-17 21:25:13 +0300

    init

---
+++ cmake-build-debug/CMakeFiles/CPP.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPP.dir/main.cpp.obj"
+  "CPP.pdb"
+  "CPP.exe"
+  "CPP.exe.manifest"
+  "libCPP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
